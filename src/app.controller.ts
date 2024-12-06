62db9302c4981fbdcb761fd5f598571a10a279ed
huynh <huynhnguyen120202@gmail.com>
2024-12-06 23:21:27 +0700

    jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,27 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
-  @Get()
-  @Render('home')
-  getHello() {
-    // console.log('check port: ', this.configService.get<string>('PORT'));
-    // return this.appService.getHello();
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  handleLogin(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
